12eabbcfb3b245d7103b7f69ab6869bce24adad6
spokey-clowwwwwwne <extremaximabanana@gmail.com>
2025-11-07 23:19:22 +0000

    Update
    Added saving and loading of image files

--- examplepp.cpp
+++ examplepp.cpp
@@ -1,11 +1,12 @@
 #include "my_image.h"
 
-// Same has C for now
 int main(int argc, char** argv)
 {
+	// Creating image
 	Image_t Image;
 	ImageInit(&Image);
 
+	// Size of image
 	unsigned int Width = 255;
 	unsigned int Height = 255;
 	unsigned short Channels = 3; // RGB
@@ -14,7 +15,7 @@ int main(int argc, char** argv)
 
 	// Use the image
 	// Example
-
+	// Assining values from 0 to 255 to each channel
 	for (unsigned int y = 0; y < Height; ++y)
 	{
 		for (unsigned int x = 0; x < Width; ++x)
@@ -28,17 +29,28 @@ int main(int argc, char** argv)
 		}
 	}
 
+	// Saving the file
+	ImageSave(&Image, "C:/Users/Usuario/C++-Programs/M.Y. Image Format/Test.my");
+
+	// Reading it back
+	Image_t NewImage;
+	ImageInit(&NewImage);
+	ImageLoad(&NewImage, "C:/Users/Usuario/C++-Programs/M.Y. Image Format/Test.my");
+
+	// Using the image
 	for (unsigned int y = 0; y < Height; ++y)
 	{
 		for (unsigned int x = 0; x < Width; ++x)
 		{
 			unsigned char* Pixel;
 
-			Pixel = (unsigned char*)ImageGetAt(&Image, x, y);
+			Pixel = (unsigned char*)ImageGetAt(&NewImage, x, y);
 
 			printf("R: %d, G: %d, B: %d\n", Pixel[0], Pixel[1], Pixel[2]);
 		}
 	}
 
+	// Freing memory
+	ImageDelete(&NewImage);
 	ImageDelete(&Image);
 }
--- my_image.h
+++ my_image.h
@@ -40,6 +40,11 @@ typedef struct
 	ui64 m_NumChunks;
 }Header_t;
 
+#define HEADER_SIGNATURE ("M.Y.")
+#define HEADER_SIGNATURE_SIZE_BYTES (sizeof(HEADER_SIGNATURE) - 1)
+// The header struct plus the signature size
+#define HEADER_SIZE_BYTES (sizeof(Header_t) + HEADER_SIGNATURE_SIZE_BYTES)
+
 typedef struct
 {
 	ui32 m_Width;
@@ -63,126 +68,126 @@ typedef enum
 typedef struct
 {
 	ChunkType_e m_ChunkType;
-	ui32 m_DataLenght;
+	ui64 m_DataLenght;
 	ui64 m_TypeSize;
 
 	void* m_Data;
 }Chunk_t;
+
+#define CHUNK_SIZE_NO_DATA (sizeof(Chunk_t) - sizeof(void*))
 
 static inline void ImageInit(Image_t* Image)
 {
 	memset(Image, 0, sizeof(Image_t));
 }
 
-static inline BOOL ImageCreate(Image_t* Image, ui32 Width, ui32 Height, ui16 Channels, ui64 TypeSize)
+static inline BOOL ImageCreate(Image_t* OutImage, ui32 Width, ui32 Height, ui16 Channels, ui64 TypeSize)
 {
-	if (!Image || Width == 0 || Height == 0 || Channels == 0 || TypeSize == 0) return FALSE;
-	if (Width == 0 || Height == 0 || Channels == 0 || TypeSize == 0) return FALSE;
+	if (!OutImage || Width == 0 || Height == 0 || Channels == 0 || TypeSize == 0) return FALSE;
 
-	if (Image->m_OwnsData && Image->m_Data)
+	if (OutImage->m_OwnsData && OutImage->m_Data)
 	{
-		free(Image->m_Data);
-		Image->m_Data = NULL;
+		free(OutImage->m_Data);
+		OutImage->m_Data = NULL;
 	}
 
-	Image->m_Width = Width;
-	Image->m_Height = Height;
-	Image->m_Channels = Channels;
-	Image->m_TypeSize = TypeSize;
+	OutImage->m_Width = Width;
+	OutImage->m_Height = Height;
+	OutImage->m_Channels = Channels;
+	OutImage->m_TypeSize = TypeSize;
 
-	Image->m_Stride = (ui64)Width * Channels * TypeSize;
-	Image->m_Data = malloc(Height * Image->m_Stride);
-	if (!Image->m_Data)
+	OutImage->m_Stride = (ui64)Width * Channels * TypeSize;
+	OutImage->m_Data = malloc(Height * OutImage->m_Stride);
+	if (!OutImage->m_Data)
 	{
 		return FALSE;
 	}
 
-	Image->m_OwnsData = TRUE;
+	OutImage->m_OwnsData = TRUE;
 
 	return TRUE;
 }
 
-static inline void ImageCreateView(Image_t* Image, ui32 Width, ui32 Height, ui16 Channels, ui64 TypeSize, void* Data)
+static inline void ImageCreateView(Image_t* OutImage, ui32 Width, ui32 Height, ui16 Channels, ui64 TypeSize, void* Data)
 {
-	if (!Image || !Data || Width == 0 || Height == 0 || Channels == 0 || TypeSize == 0) return;
+	if (!OutImage || !Data || Width == 0 || Height == 0 || Channels == 0 || TypeSize == 0) return;
 
-	if (Image->m_OwnsData && Image->m_Data)
+	if (OutImage->m_OwnsData && OutImage->m_Data)
 	{
-		free(Image->m_Data);
-		Image->m_Data = NULL;
+		free(OutImage->m_Data);
+		OutImage->m_Data = NULL;
 	}
 
-	Image->m_Width = Width;
-	Image->m_Height = Height;
-	Image->m_Channels = Channels;
-	Image->m_TypeSize = TypeSize;
+	OutImage->m_Width = Width;
+	OutImage->m_Height = Height;
+	OutImage->m_Channels = Channels;
+	OutImage->m_TypeSize = TypeSize;
 
-	Image->m_Stride = (ui64)Width * Channels * TypeSize;
-	Image->m_Data = Data;
+	OutImage->m_Stride = (ui64)Width * Channels * TypeSize;
+	OutImage->m_Data = Data;
 
-	Image->m_OwnsData = FALSE;
+	OutImage->m_OwnsData = FALSE;
 }
 
-static inline void ImageCreateMove(Image_t* Image, ui32 Width, ui32 Height, ui16 Channels, ui64 TypeSize, void** Data)
+static inline void ImageCreateMove(Image_t* OutImage, ui32 Width, ui32 Height, ui16 Channels, ui64 TypeSize, void** Data)
 {
-	if (!Image || !Data) return;
+	if (!OutImage || !Data) return;
 	if (Width == 0 || Height == 0 || Channels == 0 || TypeSize == 0) return;
 
-	if (Image->m_OwnsData && Image->m_Data)
+	if (OutImage->m_OwnsData && OutImage->m_Data)
 	{
-		free(Image->m_Data);
-		Image->m_Data = NULL;
+		free(OutImage->m_Data);
+		OutImage->m_Data = NULL;
 	}
 
-	Image->m_Width = Width;
-	Image->m_Height = Height;
-	Image->m_Channels = Channels;
-	Image->m_TypeSize = TypeSize;
+	OutImage->m_Width = Width;
+	OutImage->m_Height = Height;
+	OutImage->m_Channels = Channels;
+	OutImage->m_TypeSize = TypeSize;
 
-	Image->m_Stride = (ui64)Width * Channels * TypeSize;
-	Image->m_Data = *Data;
+	OutImage->m_Stride = (ui64)Width * Channels * TypeSize;
+	OutImage->m_Data = *Data;
 
-	Image->m_OwnsData = TRUE;
+	OutImage->m_OwnsData = TRUE;
 	*Data = NULL;
 }
 
-static inline BOOL ImageCreateCopy(Image_t* Image, ui32 Width, ui32 Height, ui16 Channels, ui64 TypeSize, void* Data)
+static inline BOOL ImageCreateCopy(Image_t* OutImage, ui32 Width, ui32 Height, ui16 Channels, ui64 TypeSize, void* Data)
 {
-	if (!Image || !Data || Width == 0 || Height == 0 || Channels == 0 || TypeSize == 0) return FALSE;
+	if (!OutImage || !Data || Width == 0 || Height == 0 || Channels == 0 || TypeSize == 0) return FALSE;
 
-	if (Image->m_OwnsData && Image->m_Data)
+	if (OutImage->m_OwnsData && OutImage->m_Data)
 	{
-		free(Image->m_Data);
-		Image->m_Data = NULL;
+		free(OutImage->m_Data);
+		OutImage->m_Data = NULL;
 	}
 
-	Image->m_Width = Width;
-	Image->m_Height = Height;
-	Image->m_Channels = Channels;
-	Image->m_TypeSize = TypeSize;
+	OutImage->m_Width = Width;
+	OutImage->m_Height = Height;
+	OutImage->m_Channels = Channels;
+	OutImage->m_TypeSize = TypeSize;
 
-	Image->m_Stride = (ui64)Width * Channels * TypeSize;
-	Image->m_Data = malloc(Height * Image->m_Stride);
-	if (!Image->m_Data)
+	OutImage->m_Stride = (ui64)Width * Channels * TypeSize;
+	OutImage->m_Data = malloc(Height * OutImage->m_Stride);
+	if (!OutImage->m_Data)
 	{
 		return FALSE;
 	}
 
-	memcpy(Image->m_Data, Data, (size_t)Image->m_Stride * Image->m_Height);
+	memcpy(OutImage->m_Data, Data, (size_t)OutImage->m_Stride * OutImage->m_Height);
 
-	Image->m_OwnsData = TRUE;
+	OutImage->m_OwnsData = TRUE;
 
 	return TRUE;
 }
 
 static inline void ImageDelete(Image_t* Image)
 {
-	if (!Image) return;
 	if (Image->m_OwnsData && Image->m_Data)
 	{
 		free(Image->m_Data);
+		Image->m_Data = NULL;
 	}
-
 	memset(Image, 0, sizeof(Image_t));
 }
 
@@ -216,5 +221,241 @@ static inline BOOL ImageSetAt(Image_t* Image, ui32 x, ui32 y, ui64 Count, const 
 	return TRUE;
 }
 
+static inline BOOL SaveToFileChunk(const Chunk_t* Chunk, FILE* Stream, const ui16 Channels)
+{
+	if (!Chunk || !Stream || Channels == 0) return FALSE;
+
+	switch (Chunk->m_ChunkType)
+	{
+	case HEADER:
+	{
+		// The header is implicit that it is the first chunk in the file and
+		// size is always known at compile time so no need to save those
+		ui8 Buffer[HEADER_SIZE_BYTES] = { 0 };
+
+		memcpy(Buffer, HEADER_SIGNATURE, HEADER_SIGNATURE_SIZE_BYTES);
+		memcpy(Buffer + HEADER_SIGNATURE_SIZE_BYTES,
+			Chunk->m_Data,
+			HEADER_SIZE_BYTES - HEADER_SIGNATURE_SIZE_BYTES);
+
+		size_t BytesWritten = fwrite(Buffer, 1, HEADER_SIZE_BYTES, Stream);
+		if (BytesWritten != HEADER_SIZE_BYTES)
+		{
+			free(Chunk->m_Data);
+			return FALSE;
+		}
+			
+		return TRUE;
+	}
+
+	case UNCOMPRESSED_DATA:
+	{
+		size_t dataBytes = (size_t)Chunk->m_DataLenght * (size_t)Chunk->m_TypeSize;
+		size_t totalBytes = dataBytes + CHUNK_SIZE_NO_DATA;
+
+		ui8* Buffer = (ui8*)malloc(totalBytes);
+		if (!Buffer) return FALSE;
+
+		// Copy the hole struct minus the void* to data
+
+		memcpy(Buffer, Chunk, CHUNK_SIZE_NO_DATA);
+		memcpy(Buffer + CHUNK_SIZE_NO_DATA, Chunk->m_Data, dataBytes);
+
+		size_t BytesWritten = fwrite(Buffer, 1, totalBytes, Stream);
+		if (BytesWritten != totalBytes)
+		{
+			free(Buffer);
+			return FALSE;
+		}
+		
+		free(Buffer);
+		return TRUE;
+	}
+
+	case RLE:
+	{
+		// Allocate enuff for the Chunk and for the one pixel to be repeated
+		size_t dataBytes = (size_t)Channels * (size_t)Chunk->m_TypeSize;
+		size_t totalBytes = dataBytes + CHUNK_SIZE_NO_DATA;
+
+		ui8* Buffer = (ui8*)malloc(totalBytes);
+		if (!Buffer)
+		{
+			return FALSE;
+		}
+
+		memcpy(Buffer, Chunk, CHUNK_SIZE_NO_DATA);
+		memcpy(Buffer + CHUNK_SIZE_NO_DATA, Chunk->m_Data, dataBytes);
+
+		size_t BytesWritten = fwrite(Buffer, 1, totalBytes, Stream);
+		if (BytesWritten != totalBytes)
+		{
+			free(Buffer);
+			return FALSE;
+		}
+
+		free(Buffer);
+		return TRUE;
+	}
+
+	default:
+		return FALSE;
+	}
+}
+
+static inline BOOL ImageSave(const Image_t* Image, const char* FileName)
+{
+	if (!Image || !FileName) return FALSE;
+
+	FILE* File = fopen(FileName, "wb");
+	if (!File) return FALSE;
+
+	// For now save raw data no RLE compression
+
+	Header_t Header =
+	{
+		.m_Width = Image->m_Width,
+		.m_Height = Image->m_Height,
+		.m_Stride = Image->m_Stride,
+		.m_Channels = Image->m_Channels,
+		.m_TypeSize = Image->m_TypeSize,
+
+		.m_NumChunks = 2 // For now only header and the raw data chunk
+	};
+
+	Chunk_t ChunkHeader = 
+	{
+		.m_ChunkType = HEADER,
+		.m_DataLenght = sizeof(Header_t),
+		.m_TypeSize = sizeof(ui8)
+	};
+	
+
+	ui8 HeaderData[sizeof(Header_t)];
+	memcpy(HeaderData, &Header, sizeof(Header_t));
+
+	ChunkHeader.m_Data = HeaderData;
+	if (!SaveToFileChunk(&ChunkHeader, File, Image->m_Channels))
+	{
+		fclose(File);
+		return FALSE;
+	}
+
+	Chunk_t ImageData =
+	{
+		.m_ChunkType = UNCOMPRESSED_DATA,
+		.m_DataLenght = Image->m_Stride * Image->m_Height,
+		.m_TypeSize = sizeof(ui8),
+		.m_Data = Image->m_Data
+	};
+	
+
+	if (!SaveToFileChunk(&ImageData, File, Image->m_Channels))
+	{
+		fclose(File);
+		return FALSE;
+	}
+
+	fclose(File);
+	return TRUE;
+}
+
+static inline BOOL ReadHeaderFromFile(Header_t* OutHeader, FILE* Stream)
+{
+	// Assuming that this function is called at the beging of the file
+
+	// Signature plus Header size
+	ui8 Buffer[HEADER_SIZE_BYTES];
+
+	if (fread(Buffer, 1, HEADER_SIZE_BYTES, Stream) != HEADER_SIZE_BYTES)
+	{
+		return FALSE;
+	}
+
+	// Check if signature is pressent "M.Y."
+	if (memcmp(Buffer, HEADER_SIGNATURE, HEADER_SIGNATURE_SIZE_BYTES) != 0)
+	{
+		return FALSE;
+	}
+
+	// Not copy the signature "M.Y."
+	memcpy(OutHeader, Buffer + HEADER_SIGNATURE_SIZE_BYTES, sizeof(Header_t));
+	return TRUE;
+}
+
+static inline BOOL ReadChunkFromFile(Chunk_t* OutChunk, FILE* Stream)
+{
+	if (fread(OutChunk, 1, CHUNK_SIZE_NO_DATA, Stream) != CHUNK_SIZE_NO_DATA)
+	{
+		return FALSE;
+	}
+
+	switch (OutChunk->m_ChunkType)
+	{
+	case UNCOMPRESSED_DATA:
+	{
+		OutChunk->m_Data = malloc(OutChunk->m_DataLenght * OutChunk->m_TypeSize);
+		if (!OutChunk->m_Data)
+		{
+			return FALSE;
+		}
+
+		if (fread(OutChunk->m_Data, OutChunk->m_TypeSize, OutChunk->m_DataLenght, Stream) != OutChunk->m_DataLenght)
+		{
+			free(OutChunk->m_Data);
+			return FALSE;
+		}
+
+		return TRUE;
+	}
+	// To be added
+	case RLE:
+	{
+		return FALSE;
+	}
+	default:
+	{
+		return FALSE;
+	}
+
+	}
+}
+
+// !!! This is only temporary !!!
+static inline BOOL ImageLoad(Image_t* OutImage, const char* FileName)
+{
+	FILE* ImageFile = fopen(FileName, "rb");
+	if (!ImageFile)
+	{
+		return FALSE;
+	}
+
+	Header_t FileHeader;
+	if (!ReadHeaderFromFile(&FileHeader, ImageFile))
+	{
+		fclose(ImageFile);
+		return FALSE;
+	}
+
+	OutImage->m_Channels = FileHeader.m_Channels;
+	OutImage->m_Width = FileHeader.m_Width;
+	OutImage->m_Height = FileHeader.m_Height;
+	OutImage->m_Stride = FileHeader.m_Stride;
+	OutImage->m_TypeSize = FileHeader.m_TypeSize;
+
+	OutImage->m_OwnsData = TRUE;
+	
+	Chunk_t Chunk;
+	if (!ReadChunkFromFile(&Chunk, ImageFile))
+	{
+		fclose(ImageFile);
+		return FALSE;
+	}
+
+	OutImage->m_Data = Chunk.m_Data;
+
+	fclose(ImageFile);
+	return TRUE;
+}
 
 #endif // MY_IMAGE
